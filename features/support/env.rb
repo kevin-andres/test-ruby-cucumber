c621c1ab39c6e75164cf2a53f8bffa6f0ac16ee5
Kevin Andres <kevin.andres@productops.com>
2016-09-22 11:50:55 -0700

    test with headlesss

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,8 +1,14 @@
 require 'hash-to-ostruct'
 require 'rspec/expectations'
 require './dsl/domain_specific_language'
-
+require 'headless'
 World(RSpec::Matchers)
+
+@headless = Headless.new
+@headless.start
+at_exit do
+  @headless.destroy
+end
 
 app = DSL::DomainSpecificLanguage.new
 
